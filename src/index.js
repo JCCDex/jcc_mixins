27d3b972a90bf85338ec5c9369a5be8a62e55d8c
GinMu <gin.musheng@gmail.com>
2018-09-12 16:26:16 +0800

    export module

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const jCurrency = require('./currency');
+const jDecimal = require('./decimal');
+const jExchangeRate = require('./exchange_rate');
+const jExchange = require('./exchange');
+const jMarketInfo = require('./market_info');
+const jNodeConfig = require('./node_config');
+
+exports = module.exports = {
+    jCurrency,
+    jDecimal,
+    jExchangeRate,
+    jExchange,
+    jMarketInfo,
+    jNodeConfig
+}
