ff75f48ff3c92b312e34714c424db45bf0af6de8
GinMu <gin.musheng@gmail.com>
2018-09-12 15:31:36 +0800

    mixins of currency

---
+++ src/currency.js
@@ -0,0 +1,25 @@
+module.exports = {
+    computed: {
+        pairs() {
+            return this.$store.getters.transactionPairs;
+        },
+        counterTitle() {
+            if (!this.pairs) {
+                return '';
+            }
+            let data = this.pairs.find(pair => {
+                return pair.counter.toLowerCase() === this.counter.toLowerCase()
+            });
+            return data ? data.counterTitle.toUpperCase() : ''
+        },
+        baseTitle() {
+            if (!this.pairs) {
+                return '';
+            }
+            let data = this.pairs.find(pair => {
+                return pair.base.toLowerCase() === this.base.toLowerCase()
+            });
+            return data ? data.baseTitle.toUpperCase() : '';
+        }
+    }
+}
